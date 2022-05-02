e42c83982f7ab038bec595a1efbd80b1936e64c6
Fernanda Romero <mf.romero.cancino@gmail.com>
2022-05-01 21:16:35 -0400

    recuperacion de datos por medio de servicios

--- src/contenedores/Index.js
+++ src/contenedores/Index.js
@@ -1,12 +1,61 @@
-import { useState } from "react";
+import {
+  useEffect,
+  useState
+} from "react";
 import Welcome from "./Welcome";
 import * as singleSpa from "single-spa";
+import { ObtenerComunas, ObtenerServicios, ObtenerTipoCobro, ObtenerTipoSuelo, ObtenerTipoVehiculo, ObtenerVehiculo } from "../servicios/servicio";
 
 
 const Index = () => {
 
   const [userData, setUserData] = useState(JSON.parse(localStorage.getItem('userData')));
   const [name, setName] = useState(userData.data.nombreUsuario.charAt(0) + userData.data.nombreUsuario.slice(1).toLowerCase());
+
+  useEffect(() => {
+    ObtenerTipoVehiculo()
+      .then((res) => {
+        localStorage.setItem('ListaTipoVehiculos', JSON.stringify(res.data));
+      }).catch((e) => {
+        localStorage.removeItem('ListaTipoVehiculos');
+      })
+  
+    ObtenerVehiculo()
+      .then((res) => {
+        localStorage.setItem('ListaVehiculos', JSON.stringify(res.data));
+      }).catch((e) => {
+        localStorage.removeItem('ListaVehiculo');
+      })
+  
+    ObtenerServicios()
+      .then((res) => {
+        localStorage.setItem('ListaServicios', JSON.stringify(res.data));
+      }).catch((e) => {
+        localStorage.removeItem('ListaServicios');
+      })
+
+    ObtenerComunas()
+      .then((res) => {
+        localStorage.setItem('ListaComunas', JSON.stringify(res.data));
+      }).catch((e) => {
+        localStorage.removeItem('ListaComunas');
+      })
+
+    ObtenerTipoCobro()
+      .then((res) => {
+        localStorage.setItem('ListaTipoCobros', JSON.stringify(res.data));
+      }).catch((e) => {
+        localStorage.removeItem('ListaTipoCobros');
+      })
+    
+    ObtenerTipoSuelo()
+      .then((res) => {
+        const arrayFiltrado = res.data.filter(suelo => suelo.idSuelo !== 0)
+        localStorage.setItem('ListaTipoSuelos', JSON.stringify(arrayFiltrado));
+      }).catch((e) => {
+        localStorage.removeItem('ListaTipoSuelos');
+      })
+  }, []);
 
   const unmountApplication = (toMf) => {
     localStorage.setItem("toMf", toMf);
@@ -20,8 +69,14 @@ const Index = () => {
   }
 
 
-  return (
-    <Welcome name={name} onContinue={onContinue}/>
+  return ( <
+    Welcome name = {
+      name
+    }
+    onContinue = {
+      onContinue
+    }
+    />
   );
 };
 
--- src/servicios/servicio.js
+++ src/servicios/servicio.js
@@ -1,20 +1,66 @@
 import axios from "axios";
 
-
-
 const headers = {
-    Accept: "application/json",
-    "Content-Type": "application/json",
-    CustomOrigin: "savecar",
-    'Access-Control-Allow-Origin': '*'
+  Accept: "application/json",
+  "Content-Type": "application/json",
+  CustomOrigin: "savecar",
+  'Access-Control-Allow-Origin': '*'
 };
 
-  
+
 export const Obtenerusuario = (correo, clave) => {
-    return axios.get(
-      `http://127.0.0.1:8000/api/obtener/usuario/correo=${correo}clave=${clave}`,
-      {
-        headers,
-      }
-    )
+  return axios.get(
+    `http://127.0.0.1:8000/api/obtener/usuario/correo=${correo}clave=${clave}`, {
+      headers,
+    }
+  )
+}
+
+export const ObtenerTipoVehiculo = () => {
+  return axios.get(
+    `http://127.0.0.1:8000/api/obtener/tipo-vehiculo`, {
+      headers,
+    }
+  )
+}
+
+
+export const ObtenerVehiculo = () => {
+  return axios.get(
+    `http://127.0.0.1:8000/api/obtener/vehiculo`, {
+      headers,
+    }
+  )
+}
+
+export const ObtenerServicios = () => {
+  return axios.get(
+    `http://127.0.0.1:8000/api/obtener/servicio`, {
+      headers,
+    }
+  )
+}
+
+export const ObtenerComunas = () => {
+  return axios.get(
+    `http://127.0.0.1:8000/api/obtener/comuna`, {
+      headers,
+    }
+  )
+}
+
+export const ObtenerTipoCobro = () => {
+  return axios.get(
+    `http://127.0.0.1:8000/api/obtener/tipo-cobro`, {
+      headers,
+    }
+  )
+}
+
+export const ObtenerTipoSuelo = () => {
+  return axios.get(
+    `http://127.0.0.1:8000/api/obtener/tipo-suelo`, {
+      headers,
+    }
+  )
 }
